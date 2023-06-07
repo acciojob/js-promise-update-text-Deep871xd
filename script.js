050f0612b14b1428b6bb29ae45c2fb0e4fe43d82
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-07 20:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function updateOutputText() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Hello, world!");
+      }, 1000);
+    });
+  }
+  
+  updateOutputText().then((message) => {
+    let outputElement = document.getElementById('output');
+    outputElement.innerText = message;
+  });
+  
